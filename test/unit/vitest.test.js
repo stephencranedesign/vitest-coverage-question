0b263fc017113ff9e5a3d9a6a608bf89fc38e05b
Stephen Russell Crane <stephenrussellcrane@gmail.com>
2024-04-09 07:31:52 -0500

    commenting out false path in vitest run and true pass in mocha run .. all code is executed but coverage report says we are missing statements / branches.

--- test/unit/vitest.test.js
+++ test/unit/vitest.test.js
@@ -1,9 +1,9 @@
 import {run} from '#src/vitest.js';
 import {equal} from 'node:assert';
 
-it('run - false', () => {
-    equal(run(false), 0);
-});
+// it('run - false', () => {
+//     equal(run(false), 0);
+// });
 
 it('run - true', () => {
     equal(run(true), 1);
